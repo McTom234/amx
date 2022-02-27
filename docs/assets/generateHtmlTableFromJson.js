f86745f8ed1b7eca662d5a93d4e385865f0d32f5
Jonas <mc_tom234@gmx.de>
2022-02-27 18:49:30 +0100

    Implement JSON to HTML Table

--- docs/assets/generateHtmlTableFromJson.js
+++ docs/assets/generateHtmlTableFromJson.js
@@ -1,7 +1,15 @@
 const jsonData = require('./example.json');
 
 function generateHtmlTableFromJson(json) {
-    console.log(json)
+    let htmlString = "";
+    for (const jsonKey in json) {
+        htmlString += `<table><tr><th>${jsonKey}</th></tr>`;
+
+        const fields = json[jsonKey];
+        for (const field in fields) htmlString += `<tr><td> - ${field} = ${fields[field]}</td></tr>`;
+        htmlString += "</table>";
+    }
+    console.log(htmlString);
 }
 
 generateHtmlTableFromJson(jsonData)
