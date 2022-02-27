af9810a38876e1c3e9a4dcdf0d18a90a6011990c
Janrupf <business.janrupf@gmail.com>
2022-02-27 11:18:46 +0100

    Finish layouting airline edit dialog

--- src/main/java/de/humboldtgym/amx/gui/EditAirlineDialog.java
+++ src/main/java/de/humboldtgym/amx/gui/EditAirlineDialog.java
@@ -10,11 +10,11 @@ public class EditAirlineDialog extends JDialog {
         setType(Type.UTILITY);
 
         setLayout(new GridBagLayout());
-        setMinimumSize(new Dimension(640, 420));
+        setMinimumSize(new Dimension(640, 0));
         ((JPanel) getContentPane()).setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
-        JPanel weightsPanel = new JPanel();
-        weightsPanel.setLayout(new BoxLayout(weightsPanel, BoxLayout.LINE_AXIS));
+        var weightsPanel = new JPanel();
+        weightsPanel.setLayout(new BoxLayout(weightsPanel, BoxLayout.X_AXIS));
 
         weightsPanel.add(new JTextField());
         weightsPanel.add(Box.createHorizontalStrut(5));
@@ -27,6 +27,27 @@ public class EditAirlineDialog extends JDialog {
         line(2, new JLabel("Secondary color"), new JButton("..."));
         line(3, new JLabel("Weight per passenger"), new JTextField());
         line(4, new JLabel("Cargo weights"), weightsPanel);
+
+        var buttonPanel = new JPanel();
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.X_AXIS));
+
+        var cancelButton = new JButton("Cancel");
+        cancelButton.setAlignmentX(JComponent.RIGHT_ALIGNMENT);
+        buttonPanel.add(cancelButton);
+        buttonPanel.add(Box.createHorizontalStrut(5));
+        var okButton = new JButton("Ok");
+        okButton.setAlignmentX(JComponent.RIGHT_ALIGNMENT);
+        buttonPanel.add(okButton);
+
+        var constraints = new GridBagConstraints();
+        constraints.gridy = 5;
+        constraints.gridwidth = 2;
+        constraints.anchor = GridBagConstraints.LINE_END;
+        constraints.insets = new Insets(5, 2, 0, 2);
+
+        add(buttonPanel, constraints);
+
+        pack();
     }
 
     private void line(int y, JComponent left, JComponent right) {
