7539846c0b1c7b3f02b61871d10cdf0d678bd8f6
Janrupf <business.janrupf@gmail.com>
2022-02-27 13:30:36 +0100

    Stub out views which are to be done

--- src/main/java/de/humboldtgym/amx/gui/DataContentView.java
+++ src/main/java/de/humboldtgym/amx/gui/DataContentView.java
@@ -2,5 +2,13 @@ package de.humboldtgym.amx.gui;
 
 import javax.swing.*;
 
-public class DataContentView extends JPanel {
+public class DataContentView extends JTabbedPane {
+    /* package */ DataContentView() {
+        addTab("Fleet", new JLabel("Fleet editor"));
+        addTab("Flights", new ToBeDoneView());
+        addTab("Routes", new ToBeDoneView());
+        addTab("Flights", new ToBeDoneView());
+        addTab("Customers", new ToBeDoneView());
+        addTab("Employees", new ToBeDoneView());
+    }
 }
---
+++ src/main/java/de/humboldtgym/amx/gui/ToBeDoneView.java
@@ -0,0 +1,11 @@
+package de.humboldtgym.amx.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ToBeDoneView extends JPanel {
+    public ToBeDoneView() {
+        setLayout(new GridBagLayout());
+        add(new JLabel("To be done, not implemented yet"));
+    }
+}
