2cfd0077e88f2d44d3735f9f2997aa8f56853f87
McTom234 <mc_tom234@gmx.de>
2022-02-27 22:47:40 +0100

    Generate table and include stylesheet

--- docs/assets/generateHtmlTableFromJson.js
+++ docs/assets/generateHtmlTableFromJson.js
@@ -1,13 +1,13 @@
-const jsonData = require('./example.json');
+const jsonData = require('./complexity.json');
 
 function generateHtmlTableFromJson(json) {
     let htmlString = "";
     for (const jsonKey in json) {
-        htmlString += `<table><tr><th>${jsonKey}</th></tr>`;
+        htmlString += `<table><tbody><tr><th>${jsonKey}</th></tr>`;
 
         const fields = json[jsonKey];
         for (const field in fields) htmlString += `<tr><td> - ${field} = ${fields[field]}</td></tr>`;
-        htmlString += "</table>";
+        htmlString += "</tbody></table>";
     }
     console.log(htmlString);
 }
